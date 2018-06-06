33065611d95999c21966095f74c22d02bb5c02d9
Chris Kim <chriskihyun@hotmail.com>
2018-06-06 23:02:01 +0000

    first

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+    let result = 0;
+
+    for (let i = 0; i < array.length; i++) {
+        if (Array.isArray(array[i])) {
+            result += sumItems(array[i])
+        } else {
+            result += array[i];
+        }
+    }
+    return result;
 }
 
+
 module.exports = sumItems;
+
+// console.log(sumItems([[[5], [[[4]]], [[3], 2]], 1, []]));
+// console.log(sumItems([[1, 2, [[3], 4]], 5, []]))
+
+
